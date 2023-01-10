5ce9237c9aca127f401de3d5c9fdd80137521315
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-01-10 17:12:46 +0100

    Added basic database API connection with basic routes

---
+++ backend/routes/drink.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const recordRoutes = express.Router();
+const dbo = require("../db/conn");
+const ObjectId = require("mongodb").ObjectId;
+
+recordRoutes.route("/drinks").get((req, res) => {
+  let db_connect = dbo.getDb("coctail_database");
+
+  db_connect
+    .collection("drinks")
+    .find({})
+    .toArray((err, docs) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({
+          error: "Wystąpił błąd podczas pobierania dokumentów z bazy danych.",
+        });
+      } else {
+        res.send(docs);
+      }
+    });
+});
+
+recordRoutes.route("/drinks/add").post(function (req, response) {
+  let db_connect = dbo.getDb("coctail_database");
+
+  db_connect
+    .collection("drinks")
+    .findOne({ name: req.body.name }, function (err, result) {
+      if (err) throw err;
+
+      if (result) {
+        response
+          .status(400)
+          .send({ error: "Drink o podanej nazwie już istnieje." });
+      } else {
+        let myobj = {
+          name: req.body.name,
+          price: req.body.price,
+          description: req.body.description,
+          quantity: req.body.quantity,
+          unit: req.body.unit,
+        };
+        db_connect.collection("drinks").insertOne(myobj, function (err, res) {
+          if (err) throw err;
+          response.json(res);
+        });
+      }
+    });
+});
+
+module.exports = recordRoutes;
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/drink"));
+
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on ${port}`);
+});
