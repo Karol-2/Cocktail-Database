3d7ba284f27d5fd8907fae897b90dc660f0768da
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-01-11 21:04:08 +0100

    Added adding and seraching comments routes

--- backend/routes/drink.js
+++ backend/routes/drink.js
@@ -79,4 +79,40 @@ recordRoutes.route("/drinks/update/:id").put(function (req, response) {
   console.log(req.params.id);
 });
 
+// ------------------------------------------------
+recordRoutes.route("/comment/add").patch(function (req, response) {
+  let db_connect = dbo.getDb("coctail_database");
+
+  let newComment = {
+    name: req.body.name,
+    comment: req.body.comment,
+    id: req.body.id,
+  };
+  db_connect
+    .collection("drinks")
+    .updateOne(
+      { _id: ObjectId(req.body.drinkId) },
+      { $push: { Comments: newComment } },
+      function (err, res) {
+        if (err) throw err;
+        console.log("Comment added");
+        response.json(res);
+      }
+    );
+});
+recordRoutes.route("/comments/:id").get(function (req, res) {
+  let db_connect = dbo.getDb("coctail_database");
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("drinks")
+    .findOne(
+      myquery,
+      { projection: { Comments: 1, _id: 0 } },
+      function (err, obj) {
+        if (err) throw err;
+        res.json(obj);
+      }
+    );
+});
+
 module.exports = recordRoutes;
